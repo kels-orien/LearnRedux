86cc7eb4cd4053a040163563730e90ee72f97348
kels-orien <kelvinorien@gmail.com>
2017-07-25 11:57:32 +0100

    Redux devtools addition

--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -1,29 +1,42 @@
 var redux = require('redux');
 
-console.log('Starting todo redux example');
+console.log('Starting redux example');
 
-var stateDefault = {
-  searchText: '',
-  showCompleted: false,
-  todos: []
-};
-var reducer = (state = stateDefault, action) => {
+var reducer = (state = {name: 'Anonymous'}, action) => {
+  // state = state || {name: 'Anonymous'};
+
   switch (action.type) {
-    case 'CHANGE_SEARCH_TEXT':
+    case 'CHANGE_NAME':
       return {
         ...state,
-        searchText: action.searchText
+        name: action.name
       };
     default:
       return state;
   }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
 
-console.log('currentState', store.getState());
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
+});
+// unsubscribe();
+
+var currentState = store.getState();
+console.log('currentState', currentState);
 
 store.dispatch({
-  type: 'CHANGE_SEARCH_TEXT',
-  searchText: 'work'
+  type: 'CHANGE_NAME',
+  name: 'Andrew'
 });
-console.log('searchText should be "work"', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Emily'
+});
